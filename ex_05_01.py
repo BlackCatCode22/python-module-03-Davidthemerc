b79f3443fff511d7f310a5aa069081b68c6dd366
David Mercado <dmerc90@gmail.com>
2025-02-20 20:43:31 -0800

    Excellent

--- ex_05_01.py
+++ ex_05_01.py
@@ -13,10 +13,12 @@ while True :
     num = num + 1
     tot = tot + fval
 
-    # Stringify values for display
-    num=str(num)
-    tot=str(tot)
+# Stringify values for display
+avg = tot/num
+num=str(num)
+tot=str(tot)
+avg=str(avg)
 
 # print ('ALL DONE')
 # print (tot,num,tot/num)
-print ('Total: ' + tot + ' # of Numbers: ' + num + 'Average of Numbers: ' + tot/num )
+print ('Total: ' + tot + ' | # of Numbers: ' + num + ' | Average of Numbers: ' + avg )
